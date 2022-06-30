9cce63f1ff2631113bad4d2440dd179da1e2f3b3
EnzeZhangSS <107888678+EnzeZhangSS@users.noreply.github.com>
2022-06-29 22:41:54 -0400

    Add files via upload

---
+++ Antra Assignments C# 1 and 2/ToPlayList/ToPlayList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToPlayList
+{
+    class ToPlayList
+    {
+        static void Main(string[] args)
+        {
+            List<string> Plist = new List<string>();
+            Console.WriteLine("This is a play list. You can add, remove or clear games.");
+            while (true)
+            {
+                Console.WriteLine("Enter command (+ game, - game, or -- to clear)):");
+                string cmd = Console.ReadLine();
+
+                if (cmd == "--")
+                {
+                    Plist.Clear();
+                    Console.WriteLine("Play list cleared.");
+                }
+                else if (cmd.Substring(0,1) == "+"){
+                    Plist.Add(cmd.Substring(2));
+                    Console.WriteLine($"Game {cmd.Substring(2)} added.");
+                }
+                else if (cmd.Substring(0,1) == "-")
+                {
+                    Plist.Remove($"{cmd.Substring(2)}");
+                    Console.WriteLine($"Game {cmd.Substring(2)} removed.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command");
+                }
+                Console.Write($"Current play list: ");
+                foreach(string i in Plist)
+                {
+                    Console.Write($"{i}, ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
