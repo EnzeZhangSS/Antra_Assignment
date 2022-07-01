41249ab96386c807e2e62fe6e114c5f0839c443d
EnzeZhangSS <107888678+EnzeZhangSS@users.noreply.github.com>
2022-06-30 22:23:56 -0400

    Add files via upload

---
+++ Antra Assignment C# 3/Fibonacci Sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+        static int Fibonacci(int i)
+        {
+            if (i == 1 || i == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(i - 2) + Fibonacci(i - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first 10 numbers of Fibonacci Sequence are: ");
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{Fibonacci(i)} ");
+            }
+        }
+    }
+}
---
+++ Antra Assignment C# 3/Reverse Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_Array
+{
+    class Program
+    {
+        static int [] GenerateNumbers()
+        {
+            Console.WriteLine("Please enter the length of array:");
+            int len = int.Parse(Console.ReadLine());
+            List<int> rList = new List<int>();
+            Random rnd = new Random();
+            for(int i = 0; i < len; i++)
+            {
+                rList.Add(rnd.Next());
+            }
+
+            return rList.ToArray();
+
+
+        }
+
+        static void Reverse(int [] num)
+        {
+            int temp;
+            for (int i = 0, j = num.Length - 1; i < j; i++,j--)
+            {
+                temp = num[j];
+                num[j] = num[i];
+                num[i] = temp;
+            }
+        }
+
+
+        static void PrintNumbers(int [] num)
+        {
+            Console.WriteLine(String.Join(",",num ));
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = GenerateNumbers();
+            Console.WriteLine("Original Array:");
+            PrintNumbers(numbers);
+            Reverse(numbers);
+            Console.WriteLine("Reversed Array:");
+            PrintNumbers(numbers);
+        }
+    }
+}
